6cda07b1cddf9830074174fb9d52cfd030c59a52
Nur Mohammad <milonsardar56@gmail.com>
2023-11-20 21:11:02 +0600

    Object using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,69 @@
 import React, { Fragment, useState } from "react";
 
 const App = () => {
-  // Count Number
+  /*1. // Count Number
   const [count, setCount] = useState(0);
   const increment = () => {
     setCount(count + 1);
   };
+  */
+  //  2. Object using useState()
+  /*
+  const [person, setPerson] = useState({
+    name: "John",
+    age: 24,
+    id: 12345,
+  });
+
+  const inputOnChange = (key, value) => {
+    setPerson((prevObj) => ({
+      ...prevObj,
+      [key]: value,
+    }));
+  };
+  */
 
   return (
     <Fragment>
-      {/*Count Number */}
+      {/* 1. //Count Number 
       <h1>{count}</h1>
-      <button onClick={increment}>Click</button>
+      <button onClick={increment}>Click</button>    
+      */}
+
+      {/*2. Input on change 
+       <h1>Person: {person["name"]}</h1>
+      <h1>Age: {person["age"]}</h1>
+      <h1>Id: {person["id"]}</h1>
+
+      
+      <label>Name</label>
+      <input
+        onChange={(e) => inputOnChange("name", e.target.value)}
+        value={person["name"]}
+        type="text"
+        name=""
+        id=""
+      />
+
+      <label>Age</label>
+      <input
+        onChange={(e) => inputOnChange("age", e.target.value)}
+        value={person["age"]}
+        type="text"
+        name=""
+        id=""
+      />
+
+      <label>Id</label>
+      <input
+        onChange={(e) => inputOnChange("id", e.target.value)}
+        value={person["id"]}
+        type="text"
+        name=""
+        id=""
+      /> */}
+
+      <h1></h1>
     </Fragment>
   );
 };
